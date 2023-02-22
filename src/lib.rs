8d00d269c4136a80a121142911f988062ee73687
Celeste Peláez <celeste@etheryal.net>
2023-02-22 01:44:15 -0500

    chore(tests): fix doc-tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,25 @@
 //! # Example
 //!
 //! ```no_run
-//! const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
-//! const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
-//! const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-//! 
+//! # use minecraft_msa_auth::MinecraftAuthorizationFlow;
+//! # use oauth2::basic::BasicClient;
+//! # use oauth2::devicecode::StandardDeviceAuthorizationResponse;
+//! # use oauth2::reqwest::async_http_client;
+//! # use oauth2::{AuthUrl, ClientId, DeviceAuthorizationUrl, Scope, TokenResponse, TokenUrl};
+//! # use reqwest::Client;
+//! # 
+//! # const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+//! # const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+//! # const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+//! # 
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # let client_id = std::env::args().nth(1).expect("client_id as first argument");
 //! let client = BasicClient::new(
 //!     ClientId::new(client_id),
 //!     None,
 //!     AuthUrl::new(MSA_AUTHORIZE_URL.to_string())?,
-//!     Some(TokenUrl::new( MSA_TOKEN_URL.to_string())?),
+//!     Some(TokenUrl::new(MSA_TOKEN_URL.to_string())?),
 //! )
 //! .set_device_authorization_url(DeviceAuthorizationUrl::new(DEVICE_CODE_URL.to_string())?);
 //! 
@@ -38,6 +48,8 @@
 //! let mc_flow = MinecraftAuthorizationFlow::new(Client::new());
 //! let mc_token = mc_flow.exchange_microsoft_token(token.access_token()).await?;
 //! println!("minecraft token: {:?}", mc_token);
+//! # Ok(())
+//! # }
 //! ```
 use std::collections::HashMap;
 
