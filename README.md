154cf98d5769c41546492b1f97000f6f186c5300
Celeste Peláez <celeste@etheryal.net>
2023-02-22 01:40:04 -0500

    chore: added `README` and crate documentation.

---
+++ README.md
@@ -0,0 +1,43 @@
+# `minecraft-msa-auth`
+
+This crate allows you to authenticate into Minecraft online services using a Microsoft Oauth2 token. You can integrate it with [oauth2-rs](https://github.com/ramosbugs/oauth2-rs) and build interactive authentication flows.
+
+# Example
+
+```rust
+const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+
+let client = BasicClient::new(
+    ClientId::new(client_id),
+    None,
+    AuthUrl::new(MSA_AUTHORIZE_URL.to_string())?,
+    Some(TokenUrl::new( MSA_TOKEN_URL.to_string())?),
+)
+.set_device_authorization_url(DeviceAuthorizationUrl::new(DEVICE_CODE_URL.to_string())?);
+
+let details: StandardDeviceAuthorizationResponse = client
+    .exchange_device_code()?
+    .add_scope(Scope::new("XboxLive.signin offline_access".to_string()))
+    .request_async(async_http_client)
+    .await?;
+
+println!(
+    "Open this URL in your browser:\n{}\nand enter the code: {}",
+    details.verification_uri().to_string(),
+    details.user_code().secret().to_string()
+);
+
+let token = client
+    .exchange_device_access_token(&details)
+    .request_async(async_http_client, tokio::time::sleep, None)
+    .await?;
+println!("microsoft token: {:?}", token);
+
+let mc_flow = MinecraftAuthorizationFlow::new(Client::new());
+let mc_token = mc_flow.exchange_microsoft_token(token.access_token()).await?;
+println!("minecraft token: {:?}", mc_token);
+```
+
+See full examples in the [examples](examples) folder.
--- examples/device_flow.rs
+++ examples/device_flow.rs
@@ -1,24 +1,24 @@
-use minecraft_microsoft_auth::MinecraftAuthorizationFlow;
+use minecraft_msa_auth::MinecraftAuthorizationFlow;
 use oauth2::basic::BasicClient;
 use oauth2::devicecode::StandardDeviceAuthorizationResponse;
 use oauth2::reqwest::async_http_client;
-use oauth2::{AuthUrl, ClientId, DeviceAuthorizationUrl, Scope, TokenUrl, TokenResponse};
+use oauth2::{AuthUrl, ClientId, DeviceAuthorizationUrl, Scope, TokenResponse, TokenUrl};
 use reqwest::Client;
+
+const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client_id = std::env::args().nth(1).expect("client_id as first argument");
-    let device_auth_url =
-        DeviceAuthorizationUrl::new("https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode".to_string())?;
     let client = BasicClient::new(
         ClientId::new(client_id),
         None,
-        AuthUrl::new("https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize".to_string())?,
-        Some(TokenUrl::new(
-            "https://login.microsoftonline.com/common/oauth2/v2.0/token".to_string(),
-        )?),
+        AuthUrl::new(MSA_AUTHORIZE_URL.to_string())?,
+        Some(TokenUrl::new(MSA_TOKEN_URL.to_string())?),
     )
-    .set_device_authorization_url(device_auth_url);
+    .set_device_authorization_url(DeviceAuthorizationUrl::new(DEVICE_CODE_URL.to_string())?);
 
     let details: StandardDeviceAuthorizationResponse = client
         .exchange_device_code()?
