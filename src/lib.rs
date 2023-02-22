78e54f044dc8e9ee62642b1b866c083e68dde43e
Celeste Peláez <celeste@etheryal.net>
2023-02-22 02:33:16 -0500

    chore(fmt): fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@
 //! # use oauth2::reqwest::async_http_client;
 //! # use oauth2::{AuthUrl, ClientId, DeviceAuthorizationUrl, Scope, TokenResponse, TokenUrl};
 //! # use reqwest::Client;
-//! # 
+//! #
 //! # const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
 //! # const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
 //! # const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-//! # 
+//! #
 //! # #[tokio::main]
 //! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
 //! # let client_id = std::env::args().nth(1).expect("client_id as first argument");
@@ -26,25 +26,25 @@
 //!     Some(TokenUrl::new(MSA_TOKEN_URL.to_string())?),
 //! )
 //! .set_device_authorization_url(DeviceAuthorizationUrl::new(DEVICE_CODE_URL.to_string())?);
-//! 
+//!
 //! let details: StandardDeviceAuthorizationResponse = client
 //!     .exchange_device_code()?
 //!     .add_scope(Scope::new("XboxLive.signin offline_access".to_string()))
 //!     .request_async(async_http_client)
 //!     .await?;
-//! 
+//!
 //! println!(
 //!     "Open this URL in your browser:\n{}\nand enter the code: {}",
 //!     details.verification_uri().to_string(),
 //!     details.user_code().secret().to_string()
 //! );
-//! 
+//!
 //! let token = client
 //!     .exchange_device_access_token(&details)
 //!     .request_async(async_http_client, tokio::time::sleep, None)
 //!     .await?;
 //! println!("microsoft token: {:?}", token);
-//! 
+//!
 //! let mc_flow = MinecraftAuthorizationFlow::new(Client::new());
 //! let mc_token = mc_flow.exchange_microsoft_token(token.access_token()).await?;
 //! println!("minecraft token: {:?}", mc_token);
@@ -54,7 +54,7 @@
 use std::collections::HashMap;
 
 use getset::{CopyGetters, Getters};
-use oauth2::{AccessToken};
+use oauth2::AccessToken;
 use reqwest::{Client as HttpClient, Response, StatusCode};
 use serde::Deserialize;
 use serde_json::json;
